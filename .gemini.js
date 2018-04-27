b9f67aa08494078049577eb3e8a3ce053c830e2d
bibushik <bibushik@yandex.ru>
2018-04-28 02:18:57 +0300

    remove gridUrl

--- .gemini.js
+++ .gemini.js
@@ -1,6 +1,6 @@
 module.exports = {
     rootUrl: 'http://localhost:3000',
-    gridUrl: 'http://127.0.0.1:4444/wd/hub',
+    //gridUrl: 'http://127.0.0.1:4444/wd/hub',
     compositeImage: true,
     retry: 10,
 
