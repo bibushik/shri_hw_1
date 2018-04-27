403bfcc17524f611bb18efbdc46c143257dae675
bibushik <bibushik@yandex.ru>
2018-04-28 02:33:46 +0300

    add edit gridUrl

--- .gemini.js
+++ .gemini.js
@@ -1,6 +1,6 @@
 module.exports = {
     rootUrl: 'http://localhost:3000',
-    //gridUrl: 'http://127.0.0.1:4444/wd/hub',
+    gridUrl: "http://darya40:H45BXpFinyKAVfMfqawW@hub-cloud.browserstack.com/wd/hub",
     compositeImage: true,
     retry: 10,
 
